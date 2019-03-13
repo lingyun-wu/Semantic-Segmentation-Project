818a3adf0779f05f12ac57bda8277eb281996ebf
Lingyun <wulingyun16@gmail.com>
2019-03-12 23:35:00 -0400

    Readme file and new main.py

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Semantic Segmentation
 ### Introduction
-In this project, you'll label the pixels of a road in images using a Fully Convolutional Network (FCN).
+In this project, the goal is to label the pixels of a road in images using a Fully Convolutional Network (FCN), which is described [here](https://people.eecs.berkeley.edu/~jonlong/long_shelhamer_fcn.pdf). The trainning data can be found [here](http://www.cvlibs.net/datasets/kitti/eval_road.php).
 
 ### Setup
-##### GPU
-`main.py` will check to make sure you are using GPU - if you don't have a GPU on your system, you can use AWS or another cloud computing platform.
+
 ##### Frameworks and Packages
 Make sure you have the following is installed:
  - [Python 3](https://www.python.org/)
@@ -18,9 +17,7 @@ You may also need [Python Image Library (PIL)](https://pillow.readthedocs.io/) f
 Download the [Kitti Road dataset](http://www.cvlibs.net/datasets/kitti/eval_road.php) from [here](http://www.cvlibs.net/download.php?file=data_road.zip).  Extract the dataset in the `data` folder.  This will create the folder `data_road` with all the training a test images.
 
 ### Start
-##### Implement
-Implement the code in the `main.py` module indicated by the "TODO" comments.
-The comments indicated with "OPTIONAL" tag are not required to complete.
+
 ##### Run
 Run the following command to run the project:
 ```
@@ -28,21 +25,6 @@ python main.py
 ```
 **Note:** If running this in Jupyter Notebook system messages, such as those regarding test status, may appear in the terminal rather than the notebook.
 
-#### Example Outputs
-Here are examples of a sufficient vs. insufficient output from a trained network:
-
-Sufficient Result          |  Insufficient Result
-:-------------------------:|:-------------------------:
-![Sufficient](./examples/sufficient_result.png)  |  ![Insufficient](./examples/insufficient_result.png)
-
-### Submission
-1. Ensure you've passed all the unit tests.
-2. Ensure you pass all points on [the rubric](https://review.udacity.com/#!/rubrics/989/view).
-3. Submit the following in a zip file.
- - `helper.py`
- - `main.py`
- - `project_tests.py`
- - Newest inference images from `runs` folder  (**all images from the most recent run**)
  
 ### Tips
 - The link for the frozen `VGG16` model is hardcoded into `helper.py`.  The model can be found [here](https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip).
@@ -55,16 +37,4 @@ In `main.py`, you'll notice that layers 3, 4 and 7 of VGG16 are utilized in crea
 
 In section 4.3, and further under header "Skip Architectures for Segmentation" and Figure 3, they note these provided for 8x, 16x and 32x upsampling, respectively. Using each of these in their FCN-8s was the most effective architecture they found. 
 
-### Optional sections
-Within `main.py`, there are a few optional sections you can also choose to implement, but are not required for the project.
 
-1. Train and perform inference on the [Cityscapes Dataset](https://www.cityscapes-dataset.com/). Note that the `project_tests.py` is not currently set up to also unit test for this alternate dataset, and `helper.py` will also need alterations, along with changing `num_classes` and `input_shape` in `main.py`. Cityscapes is a much more extensive dataset, with segmentation of 30 different classes (compared to road vs. not road on KITTI) on either 5,000 finely annotated images or 20,000 coarsely annotated images.
-2. Add image augmentation. You can use some of the augmentation techniques you may have used on Traffic Sign Classification or Behavioral Cloning, or look into additional methods for more robust training!
-3. Apply the trained model to a video. This project only involves performing inference on a set of test images, but you can also try to utilize it on a full video.
- 
-### Using GitHub and Creating Effective READMEs
-If you are unfamiliar with GitHub , Udacity has a brief [GitHub tutorial](http://blog.udacity.com/2015/06/a-beginners-git-github-tutorial.html) to get you started. Udacity also provides a more detailed free [course on git and GitHub](https://www.udacity.com/course/how-to-use-git-and-github--ud775).
-
-To learn about REAMDE files and Markdown, Udacity provides a free [course on READMEs](https://www.udacity.com/courses/ud777), as well. 
-
-GitHub also provides a [tutorial](https://guides.github.com/features/mastering-markdown/) about creating Markdown files.
--- main.py
+++ main.py
@@ -33,8 +33,17 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+   
+    # Load frozen graph layers
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return image_input, keep_prob, layer3, layer4, layer7
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -48,7 +57,67 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    weights_regularized_l2 = 1e-3
+    # Convolutional 1x1 to mantain space information.
+    conv_1x1_layer7 = tf.layers.conv2d(vgg_layer7_out,
+	    			     num_classes,
+	    			     1, # kernel_size
+	    			     padding = 'same',
+	    			     kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+	    			     kernel_regularizer= tf.contrib.layers.l2_regularizer(weights_regularized_l2),
+	    			     name='conv_1x1_layer7')
+    
+    # Upsample deconvolution
+    layer7_out = tf.layers.conv2d_transpose(conv_1x1_layer7,
+	    					  num_classes,
+	    					  4, # kernel_size
+	    					  strides= (2, 2),
+	    					  padding= 'same',
+	    					  kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+	    					  kernel_regularizer= tf.contrib.layers.l2_regularizer(weights_regularized_l2),
+	    					  name='layer7_out')
+    
+    conv_1x1_layer4 = tf.layers.conv2d(vgg_layer4_out,
+	    			     num_classes,
+	    			     1, # kernel_size
+	    			     padding = 'same',
+	    			     kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+	    			     kernel_regularizer= tf.contrib.layers.l2_regularizer(weights_regularized_l2),
+	    			     name='conv_1x1_layer4')
+
+    # Adding skip layer.
+    first_skip = tf.add(layer7_out, conv_1x1_layer4, name='first_skip')
+    
+    # Upsample deconvolutions
+    layer4_out = tf.layers.conv2d_transpose(first_skip,
+                                                   num_classes,
+                                                   4, # kernel_size
+                                                   strides= (2, 2),
+                                                   padding= 'same',
+                                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(weights_regularized_l2),
+                                                   name='layer4_out')
+    
+    conv_1x1_layer3 = tf.layers.conv2d(vgg_layer3_out,
+                                     num_classes,
+                                     1, # kernel_size
+                                     padding = 'same',
+                                     kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                     kernel_regularizer= tf.contrib.layers.l2_regularizer(weights_regularized_l2),
+                                     name='conv_1x1_layer3')
+    
+    # Adding skip layer.
+    second_skip = tf.add(layer4_out, conv_1x1_layer3, name='second_skip')
+    
+    # Upsample deconvolution
+    last_layer = tf.layers.conv2d_transpose(second_skip, num_classes, 16,
+                                                  strides= (8, 8),
+                                                  padding= 'same',
+                                                  kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                                  kernel_regularizer= tf.contrib.layers.l2_regularizer(weights_regularized_l2),
+                                                  name='last_layer')
+    
+    return last_layer
 tests.test_layers(layers)
 
 
@@ -62,7 +131,18 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+
+    # create logits and labels.
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
+    # create loss function.
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = correct_label))
+    # Define optimizer.
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    # Apply optimizer to the loss function.
+    train_op = optimizer.minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -82,14 +162,33 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+
+    print('Starting training... Total {} epochs'.format(epochs))
+    print()
+    for epoch in range(epochs):
+        print('Epoch : {}'.format(epoch + 1))
+        losses = []
+        for image, label in get_batches_fn(batch_size):
+            _, loss = sess.run([train_op, cross_entropy_loss],
+                                feed_dict={
+                                    input_image: image,
+                                    correct_label: label,
+                                    keep_prob: 0.5,
+                                    learning_rate: 0.0001
+                                })
+            losses.append('{:4f}'.format(loss))
+        print(losses)
+        print()
+    print('Training finished')
+
 tests.test_train_nn(train_nn)
 
 
 def run():
     num_classes = 2
     image_shape = (160, 576)  # KITTI dataset uses 160x576 images
-    data_dir = './data'
+    data_dir = '/data'
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
 
@@ -109,12 +208,27 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
+        # Epochs and batch size
+        epochs = 40
+        batch_size = 5
+
+        # TF variables
+        correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+
         # TODO: Build NN using load_vgg, layers, and optimize function
+        image_input, keep_prob, layer3, layer4, layer7 = load_vgg(sess, vgg_path)
+
+        last_layer = layers(layer3, layer4, layer7, num_classes)
+
+        logits, train_op, cross_entropy_loss = optimize(last_layer, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, image_input,
+             correct_label, keep_prob, learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_input)
 
         # OPTIONAL: Apply the trained model to a video
 
